179760f9d5795a8dd21a201fe2b1bc90d7a80728
TheHlafBldPrinc3 <dhruvkhatri1234@gmail.com>
2023-04-02 23:15:29 +0530

    finished TicTacToe

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 class Main
 {
     public static void main(String[] args)
@@ -12,11 +11,9 @@ class Main
                 board[row][col]=' ';
             }
         }
-
         char player='X';
         boolean gameOver = false;
         Scanner sc=new Scanner(System.in);
-
         while(!gameOver)
         {
             printBoard(board);
@@ -24,7 +21,6 @@ class Main
             int row =sc.nextInt();
             int col =sc.nextInt();
             System.out.println();
-
             if(board[row][col]==' ')
             {
                 board[row][col]=player;//Place the element
@@ -32,8 +28,50 @@ class Main
                 if(gameOver)
                 {
                     System.out.println("player "+player+" has won: ");
+                }else
+                {
+                    player=(player=='X')?'O':'x';
                 }
+            }else
+            {
+                System.out.println("Invalid move. Try again!");
             }
+        }
+        printBoard(board);
+    }
+    public static boolean haveWon(char[][] board,char player)
+    {
+        for(int row=0;row< board.length;row++)
+        {
+            if(board[row][0]==player && board[row][1]==player && board[row][2]==player)
+            {
+                return true;
+            }
+        }
+        for(int col=0;col<board[0].length;col++)
+        {
+            if(board[0][col]==player && board[1][col]==player && board[2][col]==player)
+            {
+                return true;
+            }
+        }
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true;
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            return true;
+        }
+        return false;
+    }
+    public static void printBoard(char[][] board)
+    {
+        for(int row=0;row<board.length;row++)
+        {
+            for(int col=0;col<board[row].length;row++)
+            {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
         }
     }
 }
